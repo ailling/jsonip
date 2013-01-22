7ea0023640adf86c819a7a850b03a887bdd671c6
Alan Illing <alan.illing@gmail.com>
2013-01-21 23:32:24 -0600

    jsonp handling

--- jsonip/appviews.py
+++ jsonip/appviews.py
@@ -1,55 +1,59 @@
 
 import json
 from django.http import HttpResponse
-import mimes
+from decorators import json_response
 
 def get_client_ip(request):
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(",")[0]
     else:
-        ip = request.META.get('REMOTE_ADDR')
+        ip = request.META.get("REMOTE_ADDR")
     return ip
 
 
+@json_response
 def homepage(request):
-    data = {'ip': get_client_ip(request),
-            'host': request.get_host(),
-            'request_meta': {
-                'http_referer': request.META.get('HTTP_REFERER'),
-                'content_length': request.META.get('CONTENT_LENGTH'),
-                'content_type': request.META.get('CONTENT_TYPE'),
-                'http_accept_encoding': request.META.get('HTTP_ACCEPT_ENCODING'),
-                'http_accept_language': request.META.get('HTTP_ACCEPT_LANGUAGE'),
-                'is_ajax': request.is_ajax(),
-                'method': request.method,
+    data = {"ip": get_client_ip(request),
+            "host": request.get_host(),
+            "request_meta": {
+                "http_referer": request.META.get("HTTP_REFERER"),
+                "content_length": request.META.get("CONTENT_LENGTH"),
+                "content_type": request.META.get("CONTENT_TYPE"),
+                "http_accept_encoding": request.META.get("HTTP_ACCEPT_ENCODING"),
+                "http_accept_language": request.META.get("HTTP_ACCEPT_LANGUAGE"),
+                "method": request.method,
             },
-            'pages': {
-                'home': '/',
-                'about': '/about',
+            "pages": {
+                "home": "/",
+                "about": "/about",
             }
     }
-    if request.method == 'GET':
-        data['params'] = request.GET
-    if request.method == 'POST':
-        data['params'] = request.POST
 
-    return HttpResponse(json.dumps(data), mimetype=mimes.JSON)
+    if request.method == "GET":
+        data["params"] = json.dumps(request.GET)
+    if request.method == "POST":
+        data["params"] = json.dumps(request.POST)
+    return data
 
+@json_response
 def about(request):
-    data = {'description': 'A simple network and development tool to inspect HTTP requests and initiating clients',
-            'developer': 'Alan Illing',
-            'developer-website': 'http://alanilling.com',
-            'developer-github': 'https://github.com/ailling'
+    data = {"description": "A simple network and development tool to inspect HTTP requests and initiating clients",
+            "developer": "Alan Illing",
+            "developer-website": "http://alanilling.com",
+            "developer-github": "https://github.com/ailling"
     }
-    return HttpResponse(json.dumps(data), mimetype=mimes.JSON)
+    return data
 
+@json_response
 def error404(request):
-    return HttpResponse(json.dumps({'error': '404', 'description': 'Not found'}), mimetype=mimes.JSON)
+    return {"error": "404", "description": "Not found"}
 
+@json_response
 def error500(request):
-    return HttpResponse(json.dumps({'error': '500', 'description': 'Server error'}), mimetype=mimes.JSON)
+    return {"error": "500", "description": "Server error"}
 
+@json_response
 def error403(request):
-    return HttpResponse(json.dumps({'error': '403', 'description': 'Forbidden'}), mimetype=mimes.JSON)
+    return {"error": "403", "description": "Forbidden"}
 
--- jsonip/mimes.py
+++ jsonip/mimes.py
@@ -1,1 +1,2 @@
 JSON = 'application/json'
+JSONP = 'text/javascript'
