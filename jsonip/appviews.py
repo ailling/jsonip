ebc2274ed14367c794eb143f8a3d72b098184073
Alan Illing <alan.illing@gmail.com>
2013-01-22 00:16:27 -0600

    latest

--- jsonip/appviews.py
+++ jsonip/appviews.py
@@ -15,7 +15,6 @@ def get_client_ip(request):
 @json_response
 def homepage(request):
     data = {"ip": get_client_ip(request),
-            "timestamp": str(datetime.now()),
             "request_meta": {
                 "http_referer": request.META.get("HTTP_REFERER"),
                 "content_length": request.META.get("CONTENT_LENGTH"),
